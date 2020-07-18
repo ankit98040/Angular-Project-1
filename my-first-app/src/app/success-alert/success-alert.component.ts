1c169f4db4a5477d335f5f2690cdd62613e300a2
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-07-18 07:52:08 +0530

    updates
    up1

--- my-first-app/src/app/success-alert/success-alert.component.ts
+++ my-first-app/src/app/success-alert/success-alert.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
-  styleUrls: ['./success-alert.component.css']
+  //styleUrls: ['./success-alert.component.css']
+  styles: [`
+    p {
+      padding:20px;
+      background-color: mistyrose;
+      border: 1px solid pink;
+    }
+  `]
 })
 export class SuccessAlertComponent implements OnInit {
 
